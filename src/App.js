157cc774e26ae3bc2c08eee7c51a1313ab77a8ce
suryakantkakkar <19BCS4651@cuchd.in>
2023-10-24 10:55:54 +0530

    Bug fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
       <Router>
         <Routes>
-          <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/" element={<Dashboard/>} />
         </Routes>
       </Router>
 
